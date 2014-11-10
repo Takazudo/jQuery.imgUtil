97c41918849c0be23dbc66883634b80fe4a6cd65
Takeshi Takatsudo <takazudo@gmail.com>
2014-11-10 15:47:39 +0900

    bumped version number to v0.6.1

--- jquery.imgutil.js
+++ jquery.imgutil.js
@@ -1,6 +1,6 @@
 /*! jQuery.imgUtil (https://github.com/Takazudo/jQuery.imgUtil)
  * lastupdate: 2014-11-10
- * version: 0.6.0
+ * version: 0.6.1
  * author: 'Takazudo' Takeshi Takatsudo <takazudo@gmail.com>
  * License: MIT */
 (function() {
